cb41e8fba1f3e4a6d86fe4a7068949c35bdcf3ee
Easton Madsen <eeagle25@comcast.net>
2016-02-22 09:50:57 -0700

    Added more to the cpp

--- src/Model/CtecList.cpp
+++ src/Model/CtecList.cpp
@@ -7,4 +7,32 @@
 
 #include "CtecList.h"
 
+template<class Type>
+CtecList<Type>::CtecList()
+{
+	this->size= 0;
+	this->head = nullptr;
+	this->end = nullptr;
+
+}
+
+template <class Type>
+CtecList<Type>::~CtecList()
+{
+
+}
+
+template<class Type>
+Type CtecList<Type>:: removeFromFront()
+{
+	//Create a pointer to what is after head.
+	ArrayNode<Type> * newHead = new ArrayNode<Type>();
+	newHead = head->getNext();
+	//Delete what head is pointing to
+	delete this->head;
+	//Set head to the new head
+	this->head = newHead;
+
+}
+
 

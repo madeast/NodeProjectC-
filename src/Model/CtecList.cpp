bc6aeea8f005df481d44edc1cd36cc5446a2cbef
Easton Madsen <eeagle25@comcast.net>
2016-02-24 09:41:01 -0700

    Added more to CtecList

--- src/Model/CtecList.cpp
+++ src/Model/CtecList.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "CtecList.h"
+#include "assert.h"
 
 template<class Type>
 CtecList<Type>::CtecList()
@@ -25,9 +26,15 @@ CtecList<Type>::~CtecList()
 template<class Type>
 Type CtecList<Type>:: removeFromFront()
 {
+
+	Type returnValue; //In case we need to use the value we are removing.
+
+	assert(size > 0);
 	//Create a pointer to what is after head.
 	ArrayNode<Type> * newHead = new ArrayNode<Type>();
 	newHead = head->getNext();
+	//Get what was in head node!
+	returnValue = head->getValue();
 	//Delete what head is pointing to
 	delete this->head;
 	//Set head to the new head
